6e06b19cac847a680bbfc955583dddd8651aeaf5
Hajime Hoshi <hajimehoshi@gmail.com>
2011-05-22 05:02:57 +0900

    added css and modified js

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,22 @@
-$(function ($) {
-      
-  });
+function initMainPanel($) {
+    var mainPanels = $('.mainPanel');
+    function switchMainPanel(name) {
+        mainPanels.hide();
+        var m = this.id.match(/^(.+?)NavItem$/);
+        $('#' + m[1]).show();
+        return false;
+    }
+    $('.mainPanelNavItem').click(switchMainPanel);
+}
+function initEditPanel($) {
+    var editPanels = $('.editPanel');
+    function switchEditPanel(name) {
+        editPanels.hide();
+        var m = this.id.match(/^(.+?)NavItem$/);
+        $('#' + m[1]).show();
+        return false;
+    }
+    $('.editPanelNavItem').click(switchEditPanel);
+}
+jQuery(initMainPanel);
+jQuery(initEditPanel);
--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,19 @@
-$(function ($) {
-      function createGame(e) {
-          var args = {
-              url: "/games",
-              type: "POST",
-              success: function(data, status, jqxhr) {
-                  if (jqxhr.status === 201) {
-                      location.replace(jqxhr.getResponseHeader("Location"));
-                  } else {
-                      // unexpected                             
-                  }
-              }
-          };
-          $.ajax(args);
-          return false;
-      }
-      $("#createGame").click(createGame);
-  });
+function init($) {
+    function createGame(e) {
+        var args = {
+            url: "/games",
+            type: "POST",
+            success: function(data, status, jqxhr) {
+                if (jqxhr.status === 201) {
+                    location.replace(jqxhr.getResponseHeader("Location"));
+                } else {
+                    // unexpected                             
+                }
+            }
+        };
+        $.ajax(args);
+        return false;
+    }
+    $("#createGame").click(createGame);
+}
+$(init);
